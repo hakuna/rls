8a3f01a162d504f47d8100debf3c4690127b7092
Steven Schmid <stevschmid@gmail.com>
2024-03-21 13:07:57 +0100

    Handle RESET rls.tenant_id properly

--- lib/rls/migration.rb
+++ lib/rls/migration.rb
@@ -1,12 +1,16 @@
 module RLS
   module Migration
 
+    # RESET rls.tenant_id will not remove the setting, current_setting(missing_ok=FALSE) will still return ''
+    # There is no way to remove a setting right now in PostgreSQL, so missing_ok=FALSE is not useful
+    # https://stackoverflow.com/questions/50923911/how-to-remove-configuration-parameter/50929568#50929568
     def rls_tenant_table(table_name, column: :tenant_id, role: RLS.role, &block)
       reversible do |dir|
         dir.up do
           execute <<~SQL
             ALTER TABLE "#{table_name}" ENABLE ROW LEVEL SECURITY;
-            CREATE POLICY "#{table_name}_#{role}" ON "#{table_name}" TO "#{role}" USING ("#{column}" = (current_setting('rls.tenant_id', FALSE)::bigint));
+            CREATE POLICY "#{table_name}_#{role}" ON "#{table_name}" TO "#{role}"
+              USING ("#{column}" = NULLIF(current_setting('rls.tenant_id', TRUE), '')::bigint);
           SQL
         end
         dir.down do
