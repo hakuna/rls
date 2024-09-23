f1a0de4120e5abfed44386de6ce668eff1643e6e
Steven Schmid <stevschmid@gmail.com>
2024-09-23 15:16:12 +0200

    Fix specs

--- spec/unit/rls/extensions/postgresql_adapter_spec.rb
+++ spec/unit/rls/extensions/postgresql_adapter_spec.rb
@@ -26,9 +26,11 @@ RSpec.describe RLS::Extensions::PostgreSQLAdapter do
   let(:connection) { MyAdapter.new }
 
   describe "#configure_connection" do
+    subject { -> { connection.configure_connection } }
+
     it "sets the role" do
       expect_any_instance_of(MyAdapter).to receive(:execute).with("SET ROLE 'dummy_rls_test'")
-      connection.configure_connection
+      subject.call
     end
 
     context "when admin" do
@@ -36,27 +38,31 @@ RSpec.describe RLS::Extensions::PostgreSQLAdapter do
 
       it "does not set the role" do
         expect_any_instance_of(MyAdapter).not_to receive(:execute).with("SET ROLE 'dummy_rls_test'")
-        connection # initialize
+        subject.call
       end
     end
   end
 
   describe "#rls_set" do
+    subject { -> { connection.rls_set(tenant_id: "123") } }
+
     it "sets the tenant_id" do
       expect(connection).to receive(:execute).with("SET rls.tenant_id = '123'")
-      connection.rls_set(tenant_id: "123")
+      subject.call
     end
   end
 
   describe "#rls_reset" do
+    subject { -> { connection.rls_reset } }
+
     it "resets the tenant_id" do
       expect(connection).to receive(:execute).with("RESET rls.tenant_id")
-      connection.rls_reset
+      subject.call
     end
 
     it "clears the query cache" do
       expect(connection).to receive(:clear_query_cache)
-      connection.rls_reset
+      subject.call
     end
   end
 end
